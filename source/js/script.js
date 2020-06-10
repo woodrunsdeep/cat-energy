0b0c5b43b8eb5d462fb4cb39890cac70ddf924c3
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-06-10 20:42:04 +0300

    Выносит скрипт меню в файл

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,18 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--no-js");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+  }
+});
+
 function formValidationStyling() {
   var inputsList = document.querySelectorAll(".form__text-input"); // Инициализация NodeList
   var inputsArr = Array.prototype.slice.call(inputsList); // Конвертация NodeList в Array
