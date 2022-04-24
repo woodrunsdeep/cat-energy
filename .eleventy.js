10abf491c159b80f5832c5de3be5b9299aa40b63
Gleb Dmitriev <woodrunsdeep@gmail.com>
2022-04-24 13:02:24 +0300

    build: 11ty base config

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(config) {
+    config.addPassthroughCopy('src/manifest.json');
+
+    config.addPlugin(eleventyNavigationPlugin);
+
+    config.addTransform('htmlmin', (content, outputPath) => {
+        if(outputPath && outputPath.endsWith('.html')) {
+            let htmlmin = require('html-minifier');
+            let result = htmlmin.minify(
+                content, {
+                    removeComments: true,
+                    collapseWhitespace: true
+                }
+            );
+            return result;
+        }
+        return content;
+    });
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: '_includes',
+            layouts: '_layouts',
+        },
+        dataTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        passthroughFileCopy: true,
+        templateFormats: [
+            'md', 'njk'
+        ],
+    };
+}
