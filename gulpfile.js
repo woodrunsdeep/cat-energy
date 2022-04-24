b2277663bb807b8e87800b668323e9baa6b2b24e
Gleb Dmitriev <woodrunsdeep@gmail.com>
2022-04-24 13:00:41 +0300

    style: add eslint indent rule

--- gulpfile.js
+++ gulpfile.js
@@ -15,84 +15,84 @@ const gwebp = require('gulp-webp');
 const terser = require('gulp-terser');
 
 const css = () => src('src/sass/style.scss')
-  .pipe(plumber())
-  .pipe(sourcemap.init())
-  .pipe(sass())
-  .pipe(postcss([
-    autoprefixer(),
-  ]))
-  .pipe(csso())
-  .pipe(rename('style.min.css'))
-  .pipe(sourcemap.write('.'))
-  .pipe(dest('dist/css'))
-  .pipe(bsync.stream());
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+        autoprefixer(),
+    ]))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(dest('dist/css'))
+    .pipe(bsync.stream());
 
 const refresh = (done) => {
-  bsync.reload();
-  done();
+    bsync.reload();
+    done();
 };
 
 const webp = () => src('dist/img/**/*.{png,jpg}')
-  .pipe(gwebp())
-  .pipe(dest('dist/img'));
+    .pipe(gwebp())
+    .pipe(dest('dist/img'));
 
 const images = () => src('dist/img/**/*.{png,jpg,svg,ico}')
-  .pipe(imagemin([
-    imagemin.optipng({ optimizationLevel: 3 }),
-    imagemin.mozjpeg({ progressive: true }),
-    imagemin.svgo(),
-  ]))
-  .pipe(dest('dist/img'));
+    .pipe(imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.mozjpeg({ progressive: true }),
+        imagemin.svgo(),
+    ]))
+    .pipe(dest('dist/img'));
 
 const sprite = () => src('src/img/**/*icon-*.svg')
-  .pipe(svgstore({
-    inlineSvg: true,
-  }))
-  .pipe(rename('sprite.svg'))
-  .pipe(dest('dist/img'));
+    .pipe(svgstore({
+        inlineSvg: true,
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(dest('dist/img'));
 
 const copy = () => src([
-  'src/fonts/**/*.{woff,woff2}',
-  'src/img/**',
+    'src/fonts/**/*.{woff,woff2}',
+    'src/img/**',
 ], {
-  base: 'src',
+    base: 'src',
 })
-  .pipe(dest('dist'));
+    .pipe(dest('dist'));
 
 const clean = () => del('dist');
 
 const compressJs = () => src('src/js/**/*.js')
-  .pipe(terser())
-  .pipe(dest('dist/js'));
+    .pipe(terser())
+    .pipe(dest('dist/js'));
 
 const server = () => {
-  bsync.init({
-    server: 'dist/',
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false,
-    browser: 'firefox',
-  });
-  watch('src/sass/**/*.scss', series(css, refresh));
-  watch('src/img/icon-*.svg', series(sprite, refresh));
+    bsync.init({
+        server: 'dist/',
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false,
+        browser: 'firefox',
+    });
+    watch('src/sass/**/*.scss', series(css, refresh));
+    watch('src/img/icon-*.svg', series(sprite, refresh));
 };
 
 const build = series(
-  clean,
-  copy,
-  parallel([
-    images,
-    webp,
-    css,
-    sprite,
-    compressJs,
-  ]),
+    clean,
+    copy,
+    parallel([
+        images,
+        webp,
+        css,
+        sprite,
+        compressJs,
+    ]),
 );
 
 exports.build = build;
 
 exports.dev = series(
-  build,
-  server,
+    build,
+    server,
 );
--- src/js/script.js
+++ src/js/script.js
@@ -4,45 +4,45 @@ const navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--no-js');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-  }
+    if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+    } else {
+        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add('main-nav--closed');
+    }
 });
 
 function formValidationStyling() {
-  const inputs = document.querySelectorAll('.form__text-input');
-  if (inputs) {
-    inputs.forEach((input) => {
-      input.addEventListener('blur', () => {
-        if (input.checkValidity()) {
-          input.classList.remove('form__text-input--error');
-        } else {
-          input.classList.add('form__text-input--error');
-        }
-      },
-      false);
-    });
-  }
+    const inputs = document.querySelectorAll('.form__text-input');
+    if (inputs) {
+        inputs.forEach((input) => {
+            input.addEventListener('blur', () => {
+                if (input.checkValidity()) {
+                    input.classList.remove('form__text-input--error');
+                } else {
+                    input.classList.add('form__text-input--error');
+                }
+            },
+            false);
+        });
+    }
 }
 
 function initSlider() {
-  const fatCat = document.querySelector('.slider__image-container--before');
-  const buttonBefore = document.querySelector('.slider__button--before');
-  const buttonAfter = document.querySelector('.slider__button--after');
+    const fatCat = document.querySelector('.slider__image-container--before');
+    const buttonBefore = document.querySelector('.slider__button--before');
+    const buttonAfter = document.querySelector('.slider__button--after');
 
-  if (buttonAfter && buttonBefore) {
-    buttonAfter.addEventListener('click', () => {
-      fatCat.style.width = '0';
-    });
+    if (buttonAfter && buttonBefore) {
+        buttonAfter.addEventListener('click', () => {
+            fatCat.style.width = '0';
+        });
 
-    buttonBefore.addEventListener('click', () => {
-      fatCat.style.width = '100vw';
-    });
-  }
+        buttonBefore.addEventListener('click', () => {
+            fatCat.style.width = '100vw';
+        });
+    }
 }
 
 formValidationStyling();
